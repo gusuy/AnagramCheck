5de26d8fc8a4ed4d00ad3174fdd5fe63cd4f5b66
Gus Uy <gus.mgu@gmail.com>
2015-12-16 17:53:39 -0500

    Fix new-line delimiter in reader pt.2

--- AnagramCheck/StreamReader.swift
+++ AnagramCheck/StreamReader.swift
@@ -11,7 +11,7 @@ import Foundation
 class StreamReader  {
     
     struct Constants {
-        static let delimiter = "\r"
+        static let delimiter = "\r\n"
     }
     
     let encoding : UInt
